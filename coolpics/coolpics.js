5727bd5cbd91f20f1327525aadc829bf2c6bc0c6
wilford-schaffner <wilford.schaffner@gmail.com>
2024-10-10 22:37:59 -0600

    finish popup when clicking on pictures.

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,7 +4,7 @@ function toggleMenu() {
 }
 
 function handleResize() {
-    const menu = document.querySelector(".menu");
+    const menu = document.querySelector(".menuButton");
     if (window.innerWidth > 1000) {
       menu.classList.remove("hide");
     } else {
@@ -14,3 +14,36 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+  return `
+    <div class="viewer">
+      <div class="viewer-content">
+        <button class="closeViewer">X</button>
+        <img src="${pic}" alt="${alt}">
+      </div>
+    </div>
+  `;
+}
+
+function viewHandler(event) {
+  const clickedElement = event.target;
+
+  if (clickedElement.tagName === 'IMG') {
+    const srcParts = clickedElement.src.split('-');
+    const fullSizeSrc = srcParts[0] + '-full.jpeg';
+    const altText = clickedElement.alt;
+
+    const viewerHTML = viewerTemplate(fullSizeSrc, altText);
+    document.body.insertAdjacentHTML('afterbegin', viewerHTML);
+
+    document.querySelector('.closeViewer').addEventListener('click', closeViewer);
+  }
+}
+
+function closeViewer() {
+  const viewer = document.querySelector('.viewer');
+  if (viewer) {
+    viewer.remove();
+  }
+}
