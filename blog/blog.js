dcf6a8ee56eecf996e3c1283cc1583c011412654
wilford-schaffner <wilford.schaffner@gmail.com>
2024-10-17 22:04:44 -0600

    finish blog post assignment

---
+++ blog/blog.js
@@ -0,0 +1,51 @@
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '****'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	}
+]
+
+function articleTemplate(article) {
+	const { title, date, description, imgSrc, imgAlt, ages, genre, stars } = article
+
+	return `<div class="book-info-left">
+                    <p>${date}</p>
+                    <p>${ages}</p>
+                    <p>${genre}</p>
+                    <p>${stars}</p>
+                </div>
+                <div>
+                    <h2>${title}</h2>
+                    <img src="${imgSrc}" alt="${imgAlt}"></img>
+                    <p>${description}</p>
+                </div>`
+}
+
+function displayArticles(articles) {
+	articles.forEach(article => {
+		document.querySelector(".articles").insertAdjacentHTML("beforeend", articleTemplate(article))
+	})
+}
+
+displayArticles(articles)
