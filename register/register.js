90edd86fd08ecd8ed1f9ae874284d6a1810b4f0f
wilford-schaffner <wilford.schaffner@gmail.com>
2024-11-07 15:29:27 -0700

    finish register form

--- register/register.js
+++ register/register.js
@@ -1,37 +1,50 @@
 let participantCount = 1;
 
+function participantTemplate(count) {
+    return `
+        <section class="participant${count}">
+            <p>Participant ${count}</p>
+            <div class="item">
+                <label for="fname${count}">First Name<span>*</span></label>
+                <input id="fname${count}" type="text" name="fname" value="" required />
+            </div>
+            <div class="item activities">
+                <label for="activity${count}">Activity #<span>*</span></label>
+                <input id="activity${count}" type="text" name="activity" />
+            </div>
+            <div class="item">
+                <label for="fee${count}">Fee ($)<span>*</span></label>
+                <input id="fee${count}" type="number" name="fee" />
+            </div>
+            <div class="item">
+                <label for="date${count}">Desired Date <span>*</span></label>
+                <input id="date${count}" type="date" name="date" />
+            </div>
+            <div class="item">
+                <p>Grade</p>
+                <select id="grade${count}">
+                    <option selected value="" disabled selected></option>
+                    <option value="1">1st</option>
+                    <option value="2">2nd</option>
+                    <option value="3">3rd</option>
+                    <option value="4">4th</option>
+                    <option value="5">5th</option>
+                    <option value="6">6th</option>
+                    <option value="7">7th</option>
+                    <option value="8">8th</option>
+                    <option value="9">9th</option>
+                    <option value="10">10th</option>
+                    <option value="11">11th</option>
+                    <option value="12">12th</option>
+                </select>
+            </div>
+        </section>
+    `;
+}
+
 document.getElementById('add').addEventListener('click', () => {
     participantCount++;
-    
-    const originalSection = document.querySelector('.participant1');
-    const newSection = originalSection.cloneNode(true);
-    newSection.className = `participant${participantCount}`;
-    
-    const participantText = newSection.querySelector('p');
-    participantText.textContent = `Participant ${participantCount}`;
-    
-    const elements = newSection.querySelectorAll('input, select');
-    
-    elements.forEach(element => {
-        if (element.id) {
-            const newId = `${element.id}_${participantCount}`;
-            element.id = newId;
-            
-            const label = newSection.querySelector(`label[for="${element.id.split('_')[0]}"]`);
-            if (label) {
-                label.setAttribute('for', newId);
-            }
-        }
-    });
-    
     const addButton = document.getElementById('add');
-    addButton.parentElement.insertBefore(newSection, addButton);
-    
-    newSection.querySelectorAll('input').forEach(input => {
-        input.value = '';
-    });
-    
-    newSection.querySelectorAll('select').forEach(select => {
-        select.selectedIndex = 0;
-    });
+    const template = participantTemplate(participantCount)
+    addButton.insertAdjacentHTML('beforebegin', template);
 });
